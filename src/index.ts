493abaa8da20c52200676b380599d9748b72ec5c
Stephen Fraser <stephen.fraser@digirati.com>
2020-08-23 20:42:54 +0100

    Exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ export { Describe } from './components/Describe';
 export { Expect } from './components/Expect';
 export { Test } from './components/Test';
 export { runTests, useIsVerbose } from './utility';
+export { When } from './components/When';
+export { ReactTestRenderer } from './components/ReactTestRenderer';
+export { ReactComponent } from './components/TestingLibraryReact';
